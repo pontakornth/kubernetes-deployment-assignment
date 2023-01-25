e0f32abfed35a19455b5c4be0d5bb18f41b91367
pontakornth <pontakorn61@gmail.com>
2023-01-25 10:25:24 +0700

    Update to video streaming service

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,7 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
+# Note: I don't know how to manage volume in Kubernetes yet.
+COPY ./videos ./videos
 EXPOSE ${PORT}
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 if (!process.env.PORT) {
@@ -7,8 +8,25 @@ if (!process.env.PORT) {
 
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get("/video", (req, res) => {
+  //
+  // Original video from here:
+  // https://sample-videos.com
+  //
+  const path = "./videos/SampleVideo_1280x720_1mb.mp4";
+  fs.stat(path, (err, stats) => {
+    if (err) {
+      console.error("An error occurred ");
+      res.sendStatus(500);
+      return;
+    }
+
+    res.writeHead(200, {
+      "Content-Length": stats.size,
+      "Content-Type": "video/mp4",
+    });
+    fs.createReadStream(path).pipe(res);
+  });
 });
 
 // This endpoint crashes the server.
